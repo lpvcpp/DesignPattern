31d5a94c1b8589d31387ed0b945f7b6f66f558a0
lvphc1 <vu.lephong@vn.bosch.com>
2016-12-05 12:01:10 +0100

    dpcl

--- src/Singleton/Singleton.h
+++ src/Singleton/Singleton.h
@@ -1,3 +1,15 @@
+// author: Le Phong Vu
+
+// Read this for DCLP
+// https://www.youtube.com/watch?v=c1gO9aB9nbs&feature=youtu.be&t=18m40s
+// http://preshing.com/20130930/double-checked-locking-is-fixed-in-cpp11/
+// 
+
+#include <atomic>
+#include <mutex>
+
+using namespace std;
+
 class Singleton
 {
 	private:
@@ -7,15 +19,12 @@ class Singleton
 			m_data = data; 		// Initialize data/
 		}
 		
-		~Singleton()		// Destructor if you want/
-		{
-
-		}
+		~Singleton();		// Destructor if you want/
 		
+		static atomic<Singleton*> m_instance; 	// Instance/
+		static mutex m_mtx;
 		
-		static Singleton *m_instance; 	// Instance/
-		
-		// Delete operator/
+		// Delete operator (only C++0x)/
 		Singleton(Singleton const&) = delete;             // Copy construct/
 		Singleton(Singleton&&) = delete;                  // Move construct/
 		Singleton& operator=(Singleton const&) = delete;  // Copy assignment operator/
@@ -23,14 +32,23 @@ class Singleton
 
 		int m_data; 			// Any data you want/
 	public:
-		static Singleton* GetInstance()// Get instance of class here/
-		{
-			if(!m_instance)
-			{
-				m_instance = new Singleton();
-			}
-			return m_instance;
-		}
+		static Singleton* GetInstance();// Get instance of class here/
+
 
 
 };
+
+
+atomic<Singleton*> Singleton::m_instance { nullptr };
+std::mutex Singleton::m_mtx;
+
+
+Singleton* Singleton::GetInstance() {
+  if(m_instance == nullptr) {
+    lock_guard<mutex> lock(m_mtx);
+    if(m_instance == nullptr) {
+        m_instance = new Singleton();
+    }
+  }
+  return m_instance;
+}
--- src/Singleton/main.cpp
+++ src/Singleton/main.cpp
@@ -1,3 +1,7 @@
+// How to run
+//  g++ main.cpp -o main -std=c++0x
+// ./main
+
 #include <iostream>
 #include "Singleton.h"
 using namespace std;
@@ -5,6 +9,7 @@ using namespace std;
 
 int main() {
  Singleton *instance = Singleton::GetInstance();
+ Singleton *instance2 = Singleton::GetInstance();
  
 
 }  // return 0;
